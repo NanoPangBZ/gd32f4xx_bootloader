745f744e9dc1668f118b90d2d8a0ee3f40fe0f35
庞 <3096470072@qq.com>
2023-06-09 22:53:32 +0800

    feat: ✨加入gd32f4xx的usb源码(仅cdc-acm)，编译通过，为进行测试

--- USER/main.c
+++ USER/main.c
@@ -1,60 +1,32 @@
-/*!
-    \file    main.c
-    \brief   GPIO running led demo
-
-    \version 2022-04-26, V2.0.0, demo for GD32F4xx
-*/
-
-/*
-    Copyright (c) 2022, GigaDevice Semiconductor Inc.
-
-    Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice,
-       this list of conditions and the following disclaimer in the documentation
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors
-       may be used to endorse or promote products derived from this software without
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
-OF SUCH DAMAGE.
-*/
-
 #include "gd32f4xx.h"
 #include "systick.h"
 #include <stdio.h>
 
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
+#include "cdc_acm_core.h"
 
-uint32_t temp = 0;
-
-typedef void (*app_fn)( void );
+usb_core_driver cdc_acm;
 
 void jump2app( uint32_t app_bin_addr )
 {
+    typedef void (*app_fn)( void );
     __disable_irq();
     app_fn app = (app_fn)(*(__IO  uint32_t*) (app_bin_addr + 4));
     //取app_bin_addr所在地址的第一个uint32_t为主堆栈指针寄存器的值
     __set_MSP( *(__IO uint32_t*)(app_bin_addr) );
     //jump
     app();
+}
+
+void usb_init( void )
+{
+    usbd_init (&cdc_acm,
+#ifdef USE_USB_FS
+              USB_CORE_ENUM_FS,
+#elif defined(USE_USB_HS)
+              USB_CORE_ENUM_HS,
+#endif /* USE_USB_FS */
+              &cdc_desc,
+              &cdc_class);
 }
 
 int main(void)
@@ -70,9 +42,6 @@ int main(void)
     gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_6);
     /* reset LED2 GPIO pin */
     gpio_bit_reset(GPIOC, GPIO_PIN_6);
-
-    temp = SCB->VTOR;
-
 
     uint8_t cnt = 0;
 
